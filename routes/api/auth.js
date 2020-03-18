05f50ee9b76fcb6a061d84f7b08fadc1ace7e627
James Appello <classicjamz94@yahoo.com>
2020-03-17 21:59:11 -0400

    register login and tokens work

---
+++ routes/api/auth.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+// Bring in MIDDLEWARE
+const auth = require('../../middleware/auth');
+// MODEL(S)
+const User = require('../../models/User');
+
+
+// @route   GET api/auth
+// @desc    test route
+// @access  Public
+
+router.get('/', auth, async (req,res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error!')
+    }
+});
+
+
+// ----------------
+// @route   POST api/auth
+// @desc    Authenticate user & "GET THE TOKEN"
+// @access  Public
+
+router.post('/', [
+    check('email', "Please include a valid email")
+        .isEmail(),
+    check('password', 'Password is required!')
+        .exists()        
+],
+async (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // if there are errors
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+    try {
+        //===============*|_\SECURITY/_|*==============
+        //1- see if user DOES NOT exist
+        let user = await User.findOne({ email });
+        if (!user) {
+            res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+        }
+        
+        //need to match email and password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch){
+            return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+        }
+        //===============*|_/SECURITY\_|*==============
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+        jwt.sign(payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 36000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error')
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const connectDB = require('./config/db');
 // connect database
 connectDB();
 
+// init MIDDLEWARE
+app.use(express.json({ extended: false }))
+
+// define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 app.get('/', (req,res) => res.send("API SERVER STARTED"))
 
 app.listen(PORT, () => console.log(`Express Server listening at port-> ${PORT}`))
