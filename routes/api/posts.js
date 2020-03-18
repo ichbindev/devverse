c34938a717899083b52e53100e0848835c2664b9
James Appello <classicjamz94@yahoo.com>
2020-03-18 19:04:25 -0400

    create read and delete posts next is likes and comments to conclude the serverside portion

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,7 +10,6 @@ const User = require("../../models/User");
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Private
-
 router.post(
   "/",
   [
@@ -49,5 +48,76 @@ router.post(
     }
   }
 );
+
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+    try {
+        // get the posts w/ await and find().sort()
+        const posts = await Post.find().sort({ date: -1 });
+
+        res.json(posts);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   GET api/posts/:id
+// @desc    Get posy by ID
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        // get the posts w/ await and find().sort()
+        const post = await Post.findById(req.params.id);
+        // check if there is a post with the uid
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        };
+
+        res.json(post);
+
+    } catch (err) {
+        console.error(err);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Get post by ID & remove it from db
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        // get the posts w/ await and find().sort()
+        const post = await Post.findById(req.params.id);
+
+        // Check if post exists
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' })
+        }
+
+        // Check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+        await post.remove();
+
+        res.json({ msg: 'Post removed' });
+
+    } catch (err) {
+        console.error(err);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' })
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;
