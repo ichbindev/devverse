ddd5be94d6274c220ba5eec1cdbf04e7aee77d7f
James Appello <classicjamz94@yahoo.com>
2020-03-18 16:25:50 -0400

    add a new post and created the post model as well

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+// MODEL(S)
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-// @route   GET api/posts
-// @desc    test route
-// @access  Public
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
 
-router.get('/', (req,res) => res.send('posts route'))
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    // errors object
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // * ALWAYS NEED A TRY-CATCH IN ASYNC_METHOD
+    try {
+      // create a user from the model
+      // so we can get the name and user and avatar
+      const user = await User.findById(req.user.id).select("-password");
+      // new post object and txt will come from body
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+      res.json(post)
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
