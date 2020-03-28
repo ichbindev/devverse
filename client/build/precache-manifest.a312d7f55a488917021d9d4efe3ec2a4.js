7d7ae564cf8da9c90ec0096aced5131d770056ef
James Appello <classicjamz94@yahoo.com>
2020-03-27 21:22:10 -0400

    fix deployment

---
+++ client/build/precache-manifest.a312d7f55a488917021d9d4efe3ec2a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7c499ad5257bac835fb1a9a18502b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887d969076a7a8d06fef",
+    "url": "/static/css/main.7b6c8808.chunk.css"
+  },
+  {
+    "revision": "5d244a8a366375a6a145",
+    "url": "/static/js/2.8cdacc92.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.8cdacc92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887d969076a7a8d06fef",
+    "url": "/static/js/main.35f6d716.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "cf1f92f744db6ae877890ad4b4e55418",
+    "url": "/static/media/42.cf1f92f7.gif"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
