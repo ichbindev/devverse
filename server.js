4f69b6ed87cd6ef263298b94d66c4fcb9d944fe1
James Appello <classicjamz94@yahoo.com>
2020-03-28 15:28:38 -0400

    remove build

--- server.js
+++ server.js
@@ -19,12 +19,6 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Server static assets when DEPLOYED
 if (process.env.NODE_ENV === 'production') {
     // Set static folder: index.html to be our static route to be placed in a folder called '/build'
-    /**
-     * FOR SOME REASON THIS IS MAKING THE APP NOT DEPLOY PROPERLY TO HEROKU
-     * 
-     * THO IS SAYS BUILD-SUCCESSFUL IT GIVES ME AN APPLICATION ERROR
-     * 
-     */
     app.use(express.static('client/build'));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
